705cc0cb2a06b341a66fc11d1caa5db4b469c362
John Chen <johnchen456@gmail.com>
2021-04-08 11:12:03 -0400

    checking output

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -11,4 +11,5 @@ def test_host(host):
     assert host.file("/opt/shfmt/v3.2.4/bin/shfmt").exists
 
     cmd = host.run("/opt/shfmt/v3.2.4/bin/shfmt --version")
+    print(cmd)
     assert cmd.stdout == "v3.2.4\n"
