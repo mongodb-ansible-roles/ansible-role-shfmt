30672798fa6aa27c35a4be30af261a99a6a1035f
John Chen <johnchen456@gmail.com>
2021-04-08 11:19:49 -0400

    fix: Change archtecture to amd64

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -11,5 +11,4 @@ def test_host(host):
     assert host.file("/opt/shfmt/v3.2.4/bin/shfmt").exists
 
     cmd = host.run("/opt/shfmt/v3.2.4/bin/shfmt --version")
-    print(cmd)
     assert cmd.stdout == "v3.2.4\n"
